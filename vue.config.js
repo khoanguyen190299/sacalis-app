0a4fe7938c2490366538c2ddc094eaefcf47bd62
Khoa.nguyen <khoanguyen190299@gmail.com>
2023-08-02 22:56:24 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 const sass = require('sass')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/sacalis-app/'
+  //   : '/'
+})
+
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/sacalis-app/'
     : '/'
-})
+}
